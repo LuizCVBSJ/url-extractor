4e5c6be18ca9ae7164535065ab080c70ac10a2b5
Luiz <deck@luiz-sd.lan>
2023-06-13 16:21:51 +0200

    file reestructured

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-#A program that read the url and interprets it for use
+# A program that read the url and interprets it for use
 
 url = "https://bytebank.com/exchange?currencyOrig=real"
 print(url)
 
-
-url_base = url[0:29]
+index_question = url.find("?")
+url_base = url[:index_question]
 print(url_base)
 
-url_parameters = url[29:47]
+url_parameters = url[index_question + 1:]
 print(url_parameters)
-
