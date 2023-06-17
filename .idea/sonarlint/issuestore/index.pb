070fc005e5002f576a6ee2d3f4682484598e81c6
Luiz Carlos Victor Barbosa da Silva Junior <luizcvbsj@gmail.com>
2023-06-18 00:34:01 +0200

    finalized the project, adding classes to do the money conversion as per the challenge, also added __str__, __eq__ and __len__ methods

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,5 +1,3 @@
 
-7
-main.py,f\1\f1bdda93d9a278e358509d498e17d97764c1fb29
 @
-url-extractor.py,f/4/f468ba4c356309eccc11c9a1ddb795de6af8dc0c
+url-extractor.py,f\4\f468ba4c356309eccc11c9a1ddb795de6af8dc0c
--- url-extractor.py
+++ url-extractor.py
@@ -1,31 +1,43 @@
+import re
+
+
 class ExtractorURL:
     def __init__(self, url):
         self.url = self.sanitize_url(url)
         self.validate_url()
 
-    def sanitize_url(self, url):
+    @staticmethod
+    def sanitize_url(url):
         if type(url) == str:
             return url.strip()
         else:
             return ""
 
     def validate_url(self):
-        if self.url == "":
+        if not self.url:
             raise ValueError("The url is empty")
 
+        url_pattern = re.compile("(http(s)?://)?(www.)?bytebank.com(.br)?/exchange")
+        match = url_pattern.match(self.url)
+        if not match:
+            raise ValueError("The url is invalid")
+
+    # Divides the url and returns the base
     def get_url_base(self):
-        index_questionMark = self.url.find("?")
-        url_base = self.url[:index_questionMark]
+        index_question_mark: int = self.url.find("?")
+        url_base = self.url[:index_question_mark]
         return url_base
 
+    # Divides the url and returns the parameters
     def get_url_parameters(self):
         url_parameters = self.url[self.url.find("?") + 1:]
         return url_parameters
 
-    def get_parameter_value(self, parameter_name):
+    # Returns the value of the parameter given
+    def get_parameter_value(self, target_parameter):
         url_parameters = self.get_url_parameters()
-        index_parameter = url_parameters.find(parameter_search)
-        index_parameter_value = index_parameter + len(parameter_search) + 1
+        index_parameter = url_parameters.find(target_parameter)
+        index_parameter_value = index_parameter + len(target_parameter) + 1
         url_parameters_separator = url_parameters.find("&", index_parameter_value)
         if url_parameters_separator == -1:
             url_parameters_value = url_parameters[index_parameter_value:]
@@ -33,5 +45,38 @@ class ExtractorURL:
             url_parameters_value = url_parameters[index_parameter_value:url_parameters_separator]
         return url_parameters_value
 
+    def __len__(self):
+        return len(self.url)
 
-url = ExtractorURL("https://bytebank.com/exchange?currencyDestination=dolar&currencyOrigin=real&amount=1500")
+    def __str__(self):
+        return self.url
+
+    def __eq__(self, other):
+        return self.url == other.url
+
+    def conversion(self):
+        currency_origin = self.get_parameter_value("currencyOrigin")
+        currency_destination = self.get_parameter_value("currencyDestination")
+        amount = float(self.get_parameter_value("amount"))
+        if currency_origin == "real" and currency_destination == "dollar":
+            conversion_rate = 0.19
+        elif currency_origin == "dollar" and currency_destination == "real":
+            conversion_rate = 5.30
+        else:
+            raise ValueError("The currency conversion is not available")
+
+        converted_amount = amount * conversion_rate
+        return converted_amount
+
+
+target_url = ExtractorURL("https://bytebank.com/exchange?currencyDestination=dollar&currencyOrigin=real&amount=1562")
+target_url_amount = target_url.get_parameter_value("amount")
+target_url_currency_origin = target_url.get_parameter_value("currencyOrigin")
+target_url_currency_destination = target_url.get_parameter_value("currencyDestination")
+
+print(target_url_amount)
+print(target_url_currency_origin)
+print(target_url_currency_destination)
+print("The length of the URL is", format(len(target_url)))
+print(target_url)
+print(target_url.conversion())
