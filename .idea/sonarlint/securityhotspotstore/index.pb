fee006aaa195b846b611cfe1d60f533cafb91ecb
Luiz Carlos Victor Barbosa da Silva Junior <luizcvbsj@gmail.com>
2023-06-17 16:59:39 +0200

    created url-extractor and moved program to there, starting using classes

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,3 +1,5 @@
 
+E
+venv/url-extractor.py,0\f\0f66db8fa37c5b0c5041fb018508f45d2da8be99
 7
 main.py,f\1\f1bdda93d9a278e358509d498e17d97764c1fb29
--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-# A program that read the url and interprets it for use
 
-url = "https://bytebank.com/exchange?currencyOrig=real"
-print(url)
-
-index_question = url.find("?")
-url_base = url[:index_question]
-print(url_base)
-
-url_parameters = url[index_question + 1:]
-print(url_parameters)
-
-parameter_search = "currencyOrig"
